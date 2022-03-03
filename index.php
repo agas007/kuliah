358941cd3ced3e1d987c01cc11cb7a40b56709eb
agas007 <agas9403@gmail.com>
2022-03-04 01:18:27 +0700

    initial commit for database mahasiswa

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database Mahasiswa</title>
+</head>
+
+<body>
+    <h1>Daftar Mahasiswa</h1>
+
+    <a href="tambah.php">Tambah Data Mahasiswa</a>
+
+    <input type="text" placeholder="masukkan keyword pencarian" name="search">
+    <input type="submit" value="Search" name="search">
+
+    <table>
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>NRP</th>
+            <th>Nama</th>
+            <th>Email</th>
+            <th>Jurusan</th>
+        </tr>
+
+        <!-- penginputan menggunakan PHP -->
+        <tr>
+            <td></td>
+        </tr>
+    </table>
+</body>
+
+</html>
